00b9fb3267d2236922341f4c80466d6d980c76f7
chris <chris@DESKTOP-113RR2U>
2022-11-21 06:49:14 -0300

    Carga de todos los trabajos de la guía POO

---
+++ src/pooej01libro/PooEj01Libro.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pooej01libro;
+
+import Entidades.Libro;
+import Servicios.Metodos;
+
+/**
+ *
+ * @author chris
+ */
+public class PooEj01Libro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       Metodos m1 = new Metodos();
+       Libro l1 = m1.nuevoMetodo(); // Esta es la forma para llamar al servicio donde se encuentra la lógica
+       // System.out.println(m1); 
+        
+       
+              
+        
+    }
+}
